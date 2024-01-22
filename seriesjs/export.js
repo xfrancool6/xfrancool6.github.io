8fa972beae2546a5b53adf27794fb10c5f092983
xfrancool <francoortega961@gmail.com>
2024-01-22 12:17:07 -0300

    c3

--- seriesjs/export.js
+++
@@ -1,4 +0,0 @@
-export const pelis = "https://xfrancool.github.io/imgmov/cuevanafullv2.json";
-
-export const series =
-  "https://script.google.com/macros/s/AKfycbzMk_hdMzvPXlDi2kghb51lYhi0D-MzCiL6lDGF40alFK4XoFwUtKR1JJHIQrM1Lwbp/exec";
--- webacademyjs/loader.js
+++
@@ -1,7 +0,0 @@
-/***************************** */
-
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.querySelector("body").classList.add("loaded");
-  }, 1800);
-});
